04782a853bcbae4feb8a7c31e20a3483968529b1
PlatFormPlayZ <zjrw@icloud.com>
2023-09-18 18:29:18 -0500

    Fixed data formatting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,13 @@
 INSERT INTO `gender`
-(`id`,`name`)
+    (`id`,`name`)
 VALUES
     (1, 'Male'),
     (2, 'Female'),
     (3, 'Prefer not to say');
 
+
 INSERT INTO `person`
-(`id`, `name`, `age`, `gender_id`)
+    (`id`, `name`, `age`, `gender_id`)
 VALUES
     (1, 'Amy', 16, 2),
     (2, 'Ben', 21, 1),
@@ -18,8 +19,9 @@ VALUES
     (8, 'Hil', 30, 2),
     (9, 'Ian', 18, 1);
 
+
 INSERT INTO `pizzeria`
-(`id`, `name`)
+    (`id`, `name`)
 VALUES
     (1, 'Chicago Pizza'),
     (2, 'Dominos'),
@@ -28,8 +30,9 @@ VALUES
     (5, 'Pizza Hut'),
     (6, 'Straw Hat');
 
+
 INSERT INTO `pizza`
-(`id`, `name`)
+    (`id`, `name`)
 VALUES
     (1, 'cheese'),
     (2, 'supreme'),
@@ -37,30 +40,32 @@ VALUES
     (4, 'pepperoni'),
     (5, 'sausage');
 
+
 INSERT INTO `serves`
-(`pizzaria_id`, `pizza_id`, `price`)
+    (`pizzeria_id`, `pizza_id`, `price`)
 VALUES
-    (1, 1, 7.75),
-    (1, 2, 8.5),
-    (2, 1, 9.75),
-    (2, 3, 11),
-    (3, 1, 7),
-    (3, 3, 9.25),
-    (3, 4, 9.75),
-    (3, 5, 9.5),
-    (4, 1, 7),
-    (4, 4, 8),
-    (4, 2, 8.5),
-    (5, 1, 9),
-    (5, 4, 12),
-    (5, 5, 12),
-    (5, 2, 12),
-    (6, 1, 9.25),
-    (6, 4, 8),
-    (6, 5, 9.75);
+    (1, 1, '7.75'),
+    (1, 2, '8.5'),
+    (2, 1, '9.75'),
+    (2, 3, '11'),
+    (3, 1, '7'),
+    (3, 3, '9.25'),
+    (3, 4, '9.75'),
+    (3, 5, '9.5'),
+    (4, 1, '7'),
+    (4, 4, '8'),
+    (4, 2, '8.5'),
+    (5, 1, '9'),
+    (5, 4, '12'),
+    (5, 5, '12'),
+    (5, 2, '12'),
+    (6, 1, '9.25'),
+    (6, 4, '8'),
+    (6, 5, '9.75');
+
 
 INSERT INTO `frequents`
-(`person_id`, `pizzaria_id`)
+    (`person_id`, `pizzeria_id`)
 VALUES
     (1, 5),
     (2, 1),
@@ -82,8 +87,9 @@ VALUES
     (9, 4),
     (9, 6);
 
+
 INSERT INTO `eats`
-(`person_id`, `pizza_id`)
+    (`person_id`, `pizza_id`)
 VALUES
     (1, 3),
     (1, 4),
@@ -105,3 +111,4 @@ VALUES
     (8, 2),
     (9, 2),
     (9, 4);
+
