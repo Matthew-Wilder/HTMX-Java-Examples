63c56995cdfc389676b89876b6b5461fa6cec8f4
Matthew Wilder <112603996+Matthew-Wilder@users.noreply.github.com>
2023-09-18 20:48:58 -0500

    Add Person V1 Controller/html/htmx

---
+++ src/main/java/com/mattmattica/controllers/PersonController.java
@@ -0,0 +1,35 @@
+package com.mattmattica.controllers;
+
+import com.mattmattica.entities.Person;
+import com.mattmattica.repositories.PersonRepository;
+import com.mattmattica.utils.CollectionUtility;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @GetMapping("/people/v1/summary")
+    public String getPeople(Model model) {
+        List<Person> people = CollectionUtility.listOf(personRepository.findAll());
+        model.addAttribute("people", people);
+        return "people/summary/people-v1";
+    }
+
+    @GetMapping("/person/v1/{id}/{name}")
+    public String getEats(Model model, @PathVariable("id") Integer personId) {
+        Person p = personRepository.findById(personId).get();
+        model.addAttribute("eats", p.getPizzasEaten());
+        model.addAttribute("frequents", p.getFrequentedPizzerias());
+        return "people/summary/people-v1 :: eats";
+    }
+}
